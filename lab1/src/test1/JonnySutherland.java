606bb90263d15bf35020c0e4389dc433e98e875b
SUPHA <SUPHA@172.25.136.202>
2014-04-09 03:37:35 -0700

    lab1 java basic lab

--- lab1/src/test1/JonnySutherland.java
+++ lab1/src/test1/JonnySutherland.java
@@ -1,11 +1,10 @@
 package test1;
-
 public class JonnySutherland {
 	private String name;
 	private String position;
 	private String friend[];
 	public JonnySutherland() {
-		name ="JonnySutherland";
+		name = "Jonny Sutherland";
 		position ="Goalkeeper";
 		friend =new String[]{"Joel","Ryan","Dean"};
 	}
