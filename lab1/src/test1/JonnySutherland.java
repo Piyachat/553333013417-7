792104c1b6a3f42cf04207bf6cff17ab1d636d50
SUPHA <SUPHA@172.25.136.202>
2014-04-09 17:19:02 -0700

    ijopl;k;

---
+++ lab1/src/test1/JonnySutherland.java
@@ -0,0 +1,25 @@
+package test1;
+
+public class JonnySutherland {
+	private String name;
+	private String position;
+	private String friend[];
+	public JonnySutherland() {
+		name ="JonnySutherland";
+		position ="Goalkeeper";
+		friend =new String[]{"Joel","Ryan","Dean"};
+	}
+	public JonnySutherland(int n){	
+	}
+	public String getname(){
+		return name;
+	}
+	public String getposition(){
+		return position;
+	}
+	public String[] getfriend(){
+		return friend;
+	}
+	}
+
+
